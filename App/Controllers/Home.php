c701a77d8bc32ae71bb74f472e3243fa6b072f5f
Nikhil8946 <nikhil.khandelwal@innoraft.com>
2023-04-23 22:52:49 +0530

    lets see

---
+++ App/Controllers/Home.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controllers;
+
+/**
+ * Summary of Home
+ */
+class Home
+{
+    /**
+     * Summary of register
+     * @return void
+     */
+    public function register()
+    {
+        echo "hi register ji of new exams";
+    }
+}
